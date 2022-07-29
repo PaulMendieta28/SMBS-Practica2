75b73e12ae67950401ffb8c74287ddf7ec70205f
PaulMendieta28 <mendietarios@gmail.com>
2022-07-29 14:37:26 -0600

    Actualizacion de responsive 2 (incompleto)

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -2,18 +2,18 @@
 var modal = document.getElementById('modal');
 
 var modalClose = document.getElementById('modal-close');
-modalClose.addEventListener('click', function() {
-	modal.style.display = 'none';
+modalClose.addEventListener('click', function() { 
+  modal.style.display = "none";
 });
 
 // global handler
-document.addEventListener('click', function (e) {
-	if (e.target.className.indexOf('footer-gallery-modal') !== -1) {
-		var img = e.target;
-		var modalImg = document.getElementById('modal-content');
-		var captionText = document.getElementById('modal-caption');
-		modal.style.display = 'block';
-		modalImg.src = img.src;
-		captionText.innerHTML = img.alt;
-	}
+document.addEventListener('click', function (e) { 
+  if (e.target.className.indexOf('footer-gallery-modal') !== -1) {
+      var img = e.target;
+      var modalImg = document.getElementById("modal-content");
+      var captionText = document.getElementById("modal-caption");
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      captionText.innerHTML = img.alt;
+   }
 });
